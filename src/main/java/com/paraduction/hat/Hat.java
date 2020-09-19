054e23a90614b40d490a1676b484fe6cb980f997
Noth <nothnorth@outlook.com>
2020-09-20 01:25:57 +0900

    Initial Commit.

---
+++ src/main/java/com/paraduction/hat/Hat.java
@@ -0,0 +1,17 @@
+package com.paraduction.hat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Hat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
